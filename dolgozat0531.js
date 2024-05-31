042c7795e709162eae1056ab25cae45159378ca3
Katitabogaj <83709157+Katitabogaj@users.noreply.github.com>
2024-05-31 10:21:04 +0200

    Add files via upload

---
+++ dolgozat0531.js
@@ -0,0 +1,12 @@
+function removeFirstAndLast(string) {
+//Teljes fogalmatlanság
+}
+
+console.log('1. feladat:')
+console.log(removeFirstAndLast(''))
+console.log(removeFirstAndLast('1'))
+console.log(removeFirstAndLast('A1,B2'))
+console.log(removeFirstAndLast('1,2,3'))
+console.log(removeFirstAndLast('1,2,3,4'))
+console.log(removeFirstAndLast('A1,B2,C3,D4,E5'))
+console.log(removeFirstAndLast('A,1,23,456,78,9,Z'))
